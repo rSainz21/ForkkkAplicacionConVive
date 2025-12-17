1ae48326de0c360375546994312671fb60ddb125
Manuel <mmediavillaa02@educantabria.es>
2025-12-17 16:09:36 +0100

    Añadir proyecto Spring Boot AplicacionConVive como carpeta normal

---
+++ PostmanProyecto/AplicacionConVive/src/test/java/org/example/aplicacionconvive/AplicacionConViveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aplicacionconvive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionConViveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
