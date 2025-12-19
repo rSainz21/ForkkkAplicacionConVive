2b7e2b62de4b8b0c1b49f48341b31c9def25d92b
Roberto Sainz Arjona <rsainza2401@educantabria.es>
2025-12-19 15:44:03 +0100

    Creados los packages y la kotlin file BotonesBarraInferior

--- AndroidProyecto/app/build.gradle.kts
+++ AndroidProyecto/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.androidappproyecto"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.androidappproyecto"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
