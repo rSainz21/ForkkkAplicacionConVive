86822b8b399467c11f46e7e7f65e37bccb391fd1
Miguel Ángel Quian Díaz <mquiand2401@educantabria.es>
2025-12-22 18:18:12 +0100

    creacion de clases Inquilino y Propietario para hacer controladores mañana

---
+++ WinFormsAppProyecto/Modelos/Inquilino.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Inquilino : Usuario
+    {
+        private DateOnly fecha_alta {  get; set; }
+
+    }
+}
