a1a930e8e3d7155fcda013c671c6f2c31b0e074c
Manuel.M <mmediavillaa02@educantabria.es>
2025-12-22 16:02:57 +0100

    Añadido navigation con MiPerfil

---
+++ AndroidProyecto/app/src/main/java/com/example/androidappproyecto/modelos/Usuario.kt
@@ -0,0 +1,11 @@
+package com.example.androidappproyecto.modelos
+
+data class Usuario(
+val uid: String,
+val name: String,
+val email: String,
+val role: String,
+val phone: String,
+val address: String
+)
+
