f13ecf76a89d2e6220fce5696caf12e2d1d63bb6
Miguel Ángel Quian Díaz <mquiand2401@educantabria.es>
2025-12-22 15:45:43 +0100

    PantallaHome con las tarjetas de pisos, sin datos porque no acceden a ninguna bd que recoja los datos

---
+++ AndroidProyecto/app/src/main/java/com/example/androidappproyecto/data/Pisos.kt
@@ -0,0 +1,16 @@
+package com.example.androidappproyecto.data
+
+import com.example.androidappproyecto.modelos.Piso
+
+val pisos = listOf(
+    Piso(titulo = " ",
+        direccion = " ",
+        descripcion = " ",
+        propietario = " ",
+        urlImagen = " "),
+    Piso(titulo = " ",
+        direccion = " ",
+        descripcion = " ",
+        propietario = " ",
+        urlImagen = " "),
+)
