aafceccc90939cd7ee142a7ece6fcb16f3863477
Rebeca467 <rebecacaboo14@gmail.com>
2026-02-24 02:48:53 +0100

    correccion en api

--- AppJava/src/main/java/org/example/modelos/InquilinoPropietarioId.java
+++ AppJava/src/main/java/org/example/modelos/InquilinoPropietarioId.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 public class InquilinoPropietarioId {
     private int inquilino;
     private int propietario;
-    private LocalDateTime fecha_msg;
+    private LocalDateTime fechaMsg;
     public InquilinoPropietarioId() {
     }
 
@@ -26,23 +26,23 @@ public class InquilinoPropietarioId {
         this.propietario = propietario;
     }
 
-    public LocalDateTime getFecha_msg() {
-        return fecha_msg;
+    public LocalDateTime getFechaMsg() {
+        return fechaMsg;
     }
 
-    public void setFecha_msg(LocalDateTime fecha_msg) {
-        this.fecha_msg = fecha_msg;
+    public void setFechaMsg(LocalDateTime fechaMsg) {
+        this.fechaMsg = fechaMsg;
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         InquilinoPropietarioId that = (InquilinoPropietarioId) o;
-        return inquilino == that.inquilino && propietario == that.propietario && Objects.equals(fecha_msg, that.fecha_msg);
+        return inquilino == that.inquilino && propietario == that.propietario && Objects.equals(fechaMsg, that.fechaMsg);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(inquilino, propietario, fecha_msg);
+        return Objects.hash(inquilino, propietario, fechaMsg);
     }
 }
